6a99eeac341b5c3ca1611ec1ef5dfc96b2048087
SarcasticWala <dasayan948@gmail.com>
2025-03-19 00:05:51 +0530

    FIx4

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "src/main.tsx",
-    },
-  },
-plugins: [react()],
+  plugins: [react()],
+ 
 });
 
